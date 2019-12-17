892036583b149520d905e9857ab3bd920ff8e85e
RAKESH M R <rakeshmr723@gmail.com>
2019-12-17 19:30:05 +0530

    initial commit

---
+++ www/js/index.js
@@ -0,0 +1,40 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg=>{
+        console.log('Registered',reg);
+      }).catch(err=>{
+        console.log('Registeration failed',err);
+      });
+  });
+}
+window.addEventListener('beforeinstallPrompt',(e)=>{
+  e.preventDefault();
+  deferredPrompt=e;
+  btnAdd.style.display='block';
+});
+
+
+function hosploc() {
+    var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    function onSuccess(position) {
+      var loc = "https://www.google.com/maps/search/hospital+near+me/@" + position.coords.latitude + "," + position.coords.longitude + ",16.74z";
+      window.location.href = loc;
+      //alert('Latitude:'+position.coords.latitude+'\n'+'Longitude:'+position.coords.longitude+'\n');
+    }
+    function onError(error) {
+      alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+    }
+  }
+/*
+  function hosploc() {
+    var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    function onSuccess(position) {
+      var loc = "https://www.google.com/maps/search/hospital+near+me/@" + position.coords.latitude + "," + position.coords.longitude + ",16.74z";
+      document.getElementById("loc").innerHTML += "<iframe src="+loc+" height=100%></ifreme>"
+      //alert('Latitude:'+position.coords.latitude+'\n'+'Longitude:'+position.coords.longitude+'\n');
+    }
+    function onError(error) {
+      alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+    }
+    }*/
